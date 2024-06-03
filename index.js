161f244a21282c1bbc959cc03151b6ee8125c553
Reda Dahmane <r.dahmanee@gmail.com>
2024-06-03 14:40:14 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+this.items = [];
+this.length = 0;
+  }
+  add(item) {
+this.items.push(item); 
+  this.items.sort ((a,b)=> a-b);
+this.length = this.items.length; 
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos< 0 || pos>=this.length)  {
+      throw new Error ('Outofbounds');}
+      return this.items[pos];
+    
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  max() {
+if (this.items.length ===0) {
+  throw new Error("EmptySortedList");
+} 
+  return Math.max(...this.items);
 }
 
+
+  min() {
+if (this.items.length ===0) {
+  throw new Error("EmptySortedList");
+} 
+return Math.min(...this.items);
+  }
+
+  sum() {
+    let sum=0; 
+    this.items.forEach ((el)=> sum+=el);
+    return sum; 
+  }
+
+  avg() {
+  
+    let sum=0; 
+    this.items.forEach ((el)=> sum+=el);
+   let average = sum/this.items.length; 
+   if (this.items.length ===0) {
+    throw new Error("EmptySortedList");}
+    
+   return average; 
+
+
+  }
+
+}
 module.exports = SortedList;
